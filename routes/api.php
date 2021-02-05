e73eb4b45e63b8256456c35404e8c22e35238bfe
Fran Nav <frannavmart@gmail.com>
2021-02-05 17:15:58 +0000

    Added V2 api, routes, controllers and resources ✅

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)->only(['show', 'index', 'destroy']);
+// V1
+Route::apiResource('v1/posts', PostV1::class)->only(['show', 'index', 'destroy']);
+
+// V2
+Route::apiResource('v2/posts', PostV2::class)->only(['show', 'index']);
